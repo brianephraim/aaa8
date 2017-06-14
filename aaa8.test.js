2e03d06221fca8da143aa3fbb142aab465597016
Brian Ephraim <brianephraim@gmail.com>
2017-06-14 13:58:26 -0400

    scattered from monorepo

---
+++ aaa8.test.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+import sum from 'sum';
+import aaa8 from './aaa8';
+console.log('logging from aaa8.test.js', aaa8);
+test('adds 1 + 2 to equal 3', () => {
+  expect(1 + 2).toBe(3);
+});
